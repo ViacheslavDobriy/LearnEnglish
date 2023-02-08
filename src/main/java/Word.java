faa02d61474899ae0ca655cc6e0e1f03fee57c76
Slava Dobrov <dobrov.slava@yandex.ru>
2023-02-08 18:57:49 +0300

    Append russian coding. Need to work on file.

---
+++ src/main/java/Word.java
@@ -0,0 +1,20 @@
+public abstract class Word {
+    private String english;
+    private String russian;
+
+    public String getEnglish() {
+        return english;
+    }
+
+    public void setEnglish(String english) {
+        this.english = english;
+    }
+
+    public String getRussian() {
+        return russian;
+    }
+
+    public void setRussian(String russian) {
+        this.russian = russian;
+    }
+}
---
+++ src/main/java/WorkWithFile.java
@@ -0,0 +1,6 @@
+public class WorkWithFile {
+
+    private SetEngWord newWord = new SetEngWord();
+
+    private String path = "C:\\Users\\user\\Desktop\\GeekBrains\\Java\\StudyEnglish\\src\\main\\java";
+}
