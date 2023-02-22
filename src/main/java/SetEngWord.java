61fe384911db00718d5d779a506086d7b934d344
Slava Dobrov <dobrov.slava@yandex.ru>
2023-02-22 17:54:21 +0300

    Создал readme файл

--- src/main/java/SetEngWord.java
+++ src/main/java/SetEngWord.java
@@ -8,11 +8,13 @@ public class SetEngWord extends Word{
     }
 
     public void setEngWord() {
+        System.out.println("Введите слово на английском языке, которое нужно запомнить");
         Scanner iScanner = new Scanner(System.in);
         this.setEnglish(iScanner.nextLine());
     }
 
     public void setRusWord() {
+        System.out.println("Введите перевод этого слова на русском языке");
         Scanner iScanner = new Scanner(System.in);
         this.setRussian(iScanner.nextLine());
     }
